698699acb6ab69e9be8329ea0699e83d57d1855a
jonschlinkert <dev@sellside.com>
2014-11-24 00:39:17 -0500

    adds file support

--- index.js
+++ index.js
@@ -4,39 +4,44 @@
  * Module dependencies.
  */
 
+var fs = require('fs');
 var extend = require('extend-shallow');
-var csv = require('parse-csv');
+
+/**
+ * Requires cache
+ */
+
+var requires = {};
 
 /**
  * Expose `parser`
- *
- * @type {Object}
  */
 
 var parser = module.exports;
 
 /**
- * Parse the given `str` of CSV and callback `next(err, json)`.
+ * Parse the given `str` of CSV and callback `cb(err, json)`.
  *
  * @name .parse
  * @param {String|Object} `str` The object or string to parse.
- * @param {Object|Function} `options` or `next` callback function.
- * @param {Function} `next` callback function.
+ * @param {Object|Function} `options` or `cb` callback function.
+ * @param {Function} `cb` callback function.
  * @api public
  */
 
-parser.parse = function(str, options, next) {
+parser.parse = function(str, options, cb) {
+  var csv = requires.csv || (requires.csv = require('parse-csv'));
   if (typeof options === 'function') {
-    next = options;
+    cb = options;
     options = {};
   }
 
   var opts = extend({headers: {included: true}}, options);
   try {
     opts.jsonType = opts.jsonType || 'jsonDict';
-    next(null, JSON.parse(csv.to(opts.csv, str, opts)));
+    cb(null, JSON.parse(csv.to(opts.csv, stripBOM(str), opts)));
   } catch (err) {
-    next(err);
+    cb(err);
     return;
   }
 };
@@ -50,13 +55,72 @@ parser.parse = function(str, options, next) {
  */
 
 parser.parseSync = function(str, options) {
-  options = options || {};
-
+  var csv = requires.csv || (requires.csv = require('parse-csv'));
   var opts = extend({headers: {included: true}}, options);
   try {
     opts.jsonType = opts.jsonType || 'jsonDict';
-    return JSON.parse(csv.to(opts.csv, str, opts));
+    return JSON.parse(csv.to(opts.csv, stripBOM(str), opts));
   } catch (err) {
     return err;
   }
 };
+
+/**
+ * CSV file support. Parse the given `str` of CSV and callback `cb(err, data)`.
+ *
+ * @param {String|Object} `str` The object or string to parse.
+ * @return {Object}
+ * @api public
+ */
+
+parser.parseFile = function(fp, options, cb) {
+  if (typeof options === 'function') {
+    cb = options;
+    options = {};
+  }
+
+  var opts = extend({}, options);
+  try {
+    fs.readFile(fp, 'utf8', function(err, str) {
+      parser.parse(str, opts, cb);
+    });
+  } catch (err) {
+    cb(err);
+    return;
+  }
+};
+
+/**
+ * CSV file support. Parse a file at the given `fp`.
+ *
+ * ```js
+ * parser.parseFile('foo/bar/baz.csv');
+ * ```
+ *
+ * @param {String} `fp`
+ * @param {Object} `options` Options to pass to [parse-csv]
+ * @api public
+ */
+
+parser.parseFileSync = function(fp, options) {
+  try {
+    var str = fs.readFileSync(fp, 'utf8');
+    return parser.parseSync(str, options);
+  } catch (err) {
+    return err;
+  }
+};
+
+/**
+ * Strip byte-order marks
+ *
+ * @api private
+ */
+
+function stripBOM(str) {
+  if (str[0] === '\uFEFF') {
+    return str.substring(1);
+  } else {
+    return str;
+  }
+}
